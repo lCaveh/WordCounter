b2291c0b66c06fdf504a965ae9c1985f60959158
Kaveh <lcaveh@gmail.com>
2018-11-16 11:51:27 -0800

    IsValid, ChangeToLowerCase, ChangeSentenceToWords added

--- WordCounter.Tests/Models/WordCounterTest.cs
+++ WordCounter.Tests/Models/WordCounterTest.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WordCounter;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WordCounter.Tests
 {
@@ -9,8 +11,26 @@ namespace WordCounter.Tests
     [TestMethod]
     public void IsValid_WordandSentence_True()
     {
-      WordCounter testWordCounter = new WordCounter();
+      WordCounter testWordCounter = new WordCounter("Hi","How are you.");
       Assert.AreEqual(true, testWordCounter.IsValid());
+    }
+    [TestMethod]
+    public void ChangeToLowerCase_WordandSentence_SpecificAmount()
+    {
+      WordCounter testWordCounter = new WordCounter("Hi","How are you.");
+      testWordCounter.ChangeToLowerCase();
+      Assert.AreEqual("hi", testWordCounter.GetWord());
+      Assert.AreEqual("how are you.", testWordCounter.GetSentence());
+
+    }
+    [TestMethod]
+    public void ChangeSentenceToWords_Sentence_ListOfWords()
+    {
+      WordCounter testWordCounter = new WordCounter("Hi","How are you.");
+      List<string> resultList = new List<string> {"how", "are", "you"};
+      testWordCounter.ChangeToLowerCase();
+      testWordCounter.ChangeSentenceToWords();
+      CollectionAssert.AreEqual(resultList, testWordCounter.GetWordsOfSentence());
     }
   }
 }
--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,10 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace WordCounter
 {
   public class WordCounter
   {
+    private string MainWord;
+    private string MainSentence;
+    private List<string> WordsOfSentence= new List<string>{};
+
+    public WordCounter(string userWord, string userSentence)
+    {
+      MainWord=userWord;
+      MainSentence=userSentence;
+    }
     public bool IsValid()
     {
-      return false;
+      if (MainWord.Count()>0 && MainSentence.Count()>0)
+      {
+        return true;
+      }
+      else
+      {
+        return false;
+      }
+    }
+    public void ChangeToLowerCase()
+    {
+      MainWord= MainWord.ToLower();
+      MainSentence= MainSentence.ToLower();
+    }
+    public void ChangeSentenceToWords()
+    {
+      char[] sentenceArray = MainWord.ToCharArray();
+      string sentenceWords = "";
+      for (int i=0;i<sentenceArray.Length;i++)
+      {
+        if (Char.IsLetter(sentenceArray[i]))
+        {
+          sentenceWords+= Char.ToString(sentenceArray[i]);
+        } else if (Char.IsWhiteSpace(sentenceArray[i]) || i==sentenceArray.Length-1)
+        {
+          WordsOfSentence.Add(sentenceWords);
+          sentenceWords="";
+        }
+      }
+    }
+    public string GetWord()
+    {
+      return MainWord;
+    }
+    public string GetSentence()
+    {
+      return MainSentence;
+    }
+    public List<string> GetWordsOfSentence()
+    {
+      return WordsOfSentence;
     }
   }
 }
